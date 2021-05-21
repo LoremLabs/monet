83bc2d974e59c91b66b35bf0446715ad10d785f1
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-05-21 20:15:40 +0530

    cleanup config

--- .config/storybook/main.js
+++ .config/storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
 	stories: [
-		"../src/**/*.stories.mdx",
-		"../src/**/*.stories.@(js|jsx|ts|tsx|svelte)",
+		"../../src/**/*.stories.mdx",
+		"../../src/**/*.stories.@(js|jsx|ts|tsx|svelte)",
 	],
 	addons: [
 		"@storybook/addon-links",
@@ -17,6 +17,6 @@ module.exports = {
 		"@storybook/addon-svelte-csf",
 	],
 	svelteOptions: {
-		preprocess: require("../svelte.config.cjs").preprocess,
+		preprocess: require("../../svelte.config.js").preprocess,
 	},
 };
