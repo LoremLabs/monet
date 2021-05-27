f7e756735aa6dc637e1317e8fa247bc313740e9e
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-05-28 02:42:51 +0530

    refactor: split out BasicToast

---
+++ src/components/BasicToast.stories.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+	import { action } from "@storybook/addon-actions";
+	import BasicToast from "./BasicToast.svelte";
+
+</script>
+
+<Meta title="Components/BasicToast" component={BasicToast} argTypes={{}} />
+
+<Template let:args>
+	<BasicToast {...args} on:click={action("Close button click")} />
+</Template>
+
+<Story
+	name="Primary"
+	args={{
+		text: "This site, is web-monetized.",
+	}}
+/>
---
+++ src/components/BasicToast.svelte
@@ -0,0 +1,30 @@
+<script>
+	import "../assets/global.css";
+	import WebMonetizationLogo from "../atoms/WebMonetizationLogo.svelte";
+	import CloseIcon from "../assets/icons/close.svelte";
+
+	export let text = "This site is web-monetized.";
+
+</script>
+
+<div class="relative w-max max-w-md mt-6 mr-6">
+	<div class="bg-gray-900 w-11/12 h-4 rounded-t-2xl" />
+	<button
+		type="button"
+		title="Close"
+		class="border-none bg-white absolute -top-5 -right-5 h-8 w-8 z-10 p-1 shadow-xl-all rounded-full flex place-self-center cursor-pointer"
+		on:click
+	>
+		<CloseIcon />
+	</button>
+	<div
+		class="bg-white w-full overflow-hidden font-sans shadow-xl-all rounded-2xl rounded-tl-none inline-flex gap-4 py-4 items-center"
+	>
+		<div class="pl-4 text-gray-900 text-xl">
+			{text}
+		</div>
+		<div class="flex-shrink-0 flex p-4 bg-gray-900 rounded-l-full">
+			<WebMonetizationLogo class="h-10 w-10 text-wm-green" animate />
+		</div>
+	</div>
+</div>
