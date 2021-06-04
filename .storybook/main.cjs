fd8fd53a8dc970ba63c0fd28f78c5b8a27c18ad9
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-06-04 19:43:36 +0530

    setup static-site build, cleanup config

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,15 +1,14 @@
+const preprocess = require('svelte-preprocess');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-postcss",
-    "@storybook/addon-svelte-csf"
-  ],
-  "svelteOptions": {
-    "preprocess": require("./svelte.config.cjs").preprocess
-  }
-}
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-postcss',
+		'@storybook/addon-svelte-csf'
+	],
+	svelteOptions: {
+		preprocess: preprocess({ postcss: true })
+	}
+};
--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
-import preprocess from "svelte-preprocess";
+import preprocess from 'svelte-preprocess';
+import svelteStatic from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		preprocess({
-			postcss: true
-		}),
-	],
+	preprocess: [preprocess({ postcss: true })],
 	kit: {
+		adapter: svelteStatic(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
