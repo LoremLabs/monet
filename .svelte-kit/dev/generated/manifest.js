988e7f2b9d5036d6c6413ceec420ca5c288b513c
Matt Mankins <mankins@gmail.com>
2021-06-04 13:02:10 +0100

    needed?

--- .svelte-kit/dev/generated/manifest.js
+++ .svelte-kit/dev/generated/manifest.js
@@ -1,6 +1,6 @@
 const c = [
 	() => import("../../../src/routes/__layout.svelte"),
-	() => import("../components/error.svelte"),
+	() => import("../../../src/routes/__error.svelte"),
 	() => import("../../../src/routes/index.svelte")
 ];
 
