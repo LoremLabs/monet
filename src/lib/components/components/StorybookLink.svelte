676b37762d968fc22bfa6c483b2ad843ca7002f6
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-06-16 20:52:01 +0530

    fix(StorybookLink): use <a> with a onMount workaround

--- src/lib/components/components/StorybookLink.svelte
+++ src/lib/components/components/StorybookLink.svelte
@@ -1,15 +1,22 @@
 <script>
+	import { onMount } from 'svelte';
+
 	export let path;
+
+	let elem;
+	onMount(() => {
+		elem.href = elem.dataset.href;
+	});
 </script>
 
-<!-- We want to use a `<a>` here, but bugs!
+<!-- We want to use a `<a href="">` here, but bugs!
 	https://github.com/sveltejs/kit/pull/1679 -->
 
-<button
+<a
 	class="inline-flex gap-1 bg-pink-50 hover:bg-pink-100 focus:bg-pink-100 focus:ring-1 focus:outline-none focus:ring-pink-500 text-pink-700 p-2 rounded-md border-none"
-	type="button"
-	on:click={() => {
-		window.location.href = `/storybook/?path=${path}`;
-		return false;
-	}}><img class="w-6 h-6" src="/storybook.ico" alt="" /> View on Storybook</button
+	bind:this={elem}
+	rel="external"
+	href="#{path}"
+	data-href="/storybook/?path={path}"
+	><img class="w-6 h-6" src="/storybook.ico" alt="" /> View on Storybook</a
 >
