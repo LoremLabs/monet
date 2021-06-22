6c450b2c4620521351e4d489408e67a162d84be5
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-06-23 00:44:42 +0530

    refactor(Home): extract Example component out of Examples

---
+++ src/lib/components/home/Example.svelte
@@ -0,0 +1,33 @@
+<script>
+	import ExampleHeader from './ExampleHeader.svelte';
+
+	export let name;
+	export let title;
+	export let imageUrl;
+	export let backgroundImageUrl;
+	/** @type {"left" | "center" | "right"} */
+	export let align = 'left';
+</script>
+
+<section
+	class="px-4 md:px-10 {{
+		left: 'py-12',
+		center: 'py-24',
+		right: 'py-12',
+	}[align]} bg-no-repeat bg-contain flex flex-col gap-2 {{
+		left: 'md:flex-row-reverse',
+		center: 'items-center',
+		right: 'md:flex-row',
+	}[align]} {$$props.class}"
+	style="background-image: url({backgroundImageUrl});"
+>
+	<div class="p-5 {align !== 'center' ? 'md:mt-16' : ''}">
+		<ExampleHeader {name} {title} />
+		<p class="m-0 text-lg max-w-prose text-gray-700">
+			<slot name="description" />
+		</p>
+	</div>
+	<a class="focus:outline-none md:w-2/3" tabindex="-1" href="/components/{name}">
+		<img class="drop-shadow-2xl" src={imageUrl} alt="" />
+	</a>
+</section>
--- src/lib/components/home/Examples.svelte
+++ src/lib/components/home/Examples.svelte
@@ -1,49 +1,43 @@
 <script>
-	import ExampleHeader from './ExampleHeader.svelte';
+	import Example from './Example.svelte';
 </script>
 
-<section
-	class="py-24 px-4 md:px-10 bg-no-repeat bg-contain bg-center flex flex-col md:flex-row-reverse gap-2"
-	style="background-image: url(/svg/gradient-odd.svg);"
+<Example
+	name="monet-inline"
+	title="Inline Monetizer"
+	imageUrl="/img/inline.png"
+	align="left"
+	backgroundImageUrl="/svg/gradient-odd.svg"
+	class="bg-center"
 >
-	<div class="md:mt-16 p-5">
-		<ExampleHeader name="monet-inline" title="Inline Monetizer" />
-		<p class="m-0 text-lg max-w-prose text-gray-700">
-			This design component prompts users while reading an article.
-		</p>
-	</div>
-	<a class="focus:outline-none md:w-2/3" tabindex="-1" href="/components/monet-inline">
-		<img class="drop-shadow-2xl" src="/img/inline.png" alt="" />
-	</a>
-</section>
+	<svelte:fragment slot="description">
+		This design component prompts users while reading an article.
+	</svelte:fragment>
+</Example>
 
-<section
-	class="py-12 px-4 md:px-10 bg-no-repeat bg-contain bg-bottom flex flex-col items-center gap-2"
-	style="background-image: url(/svg/gradient-circle-blue.svg);"
+<Example
+	name="monet-riser"
+	title="Riser"
+	imageUrl="/img/riser.png"
+	align="center"
+	backgroundImageUrl="/svg/gradient-circle-blue.svg"
+	class="bg-bottom"
 >
-	<div class="p-5">
-		<ExampleHeader name="monet-riser" title="Riser" />
-		<p class="m-0 text-lg max-w-prose text-gray-700">
-			This component, at the bottom of your page, encourages users to enable web monetization.
-		</p>
-	</div>
-	<a class="focus:outline-none md:w-2/3" tabindex="-1" href="/components/monet-riser">
-		<img class="drop-shadow-2xl" src="/img/riser.png" alt="" />
-	</a>
-</section>
+	<svelte:fragment slot="description">
+		This component, at the bottom of your page, encourages users to enable web monetization.
+	</svelte:fragment>
+</Example>
 
-<section
-	class="py-24 px-4 md:px-10 bg-no-repeat bg-contain bg-right flex flex-col md:flex-row gap-2"
-	style="background-image: url(/svg/gradient-circle-orange.svg);"
+<Example
+	name="monet-modal"
+	title="Modal"
+	imageUrl="/img/modal.png"
+	align="right"
+	backgroundImageUrl="/svg/gradient-circle-orange.svg"
+	class="bg-right"
 >
-	<div class="md:mt-16 p-5">
-		<ExampleHeader name="monet-modal" title="Modal" />
-		<p class="m-0 text-lg max-w-prose text-gray-700">
-			To temporarily block access to a piece of content, this Modal prompt directs users to activate
-			Web Monetization before continuing.
-		</p>
-	</div>
-	<a class="focus:outline-none md:w-2/3" tabindex="-1" href="/components/monet-modal">
-		<img class="drop-shadow-2xl" src="/img/modal.png" alt="" />
-	</a>
-</section>
+	<svelte:fragment slot="description">
+		To temporarily block access to a piece of content, this Modal prompt directs users to activate
+		Web Monetization before continuing.
+	</svelte:fragment>
+</Example>
