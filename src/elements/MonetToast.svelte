d1c10b75254f57e9c4975927c26bd28080d98cf8
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-07-14 20:21:12 +0530

    refactor(MonetToast): don't use writeable to store collapsed state

--- src/elements/MonetToast.svelte
+++ src/elements/MonetToast.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
 	import { onMount } from "svelte";
-	import { writable } from "svelte/store";
 	import MonetToast from "../components/MonetToast/MonetToast.svelte";
 	import HiddenOnScroll from "../utils/HiddenOnScroll.svelte";
 
-	let collapsed = writable(false);
+	let collapsed = false;
 	onMount(() => {
 		const timeout = 4000;
 		// Go to small state after a while to reduce distraction
-		setTimeout(() => collapsed.set(true), timeout);
+		setTimeout(() => (collapsed = true), timeout);
 	});
 </script>
 
 <HiddenOnScroll>
-	<MonetToast collapsed={$collapsed} />
+	<MonetToast {collapsed} />
 </HiddenOnScroll>
