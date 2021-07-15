178ea9d8f4f9792ded87700e3a6fe86f16162d18
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-07-15 18:50:10 +0530

    feat(monet-pref): add monet-pref custom element (#4)

---
+++ src/elements/MonetPref.svelte
@@ -0,0 +1,14 @@
+<svelte:options accessors />
+
+<script lang="ts">
+	import type { Options } from "../components/MonetPref/MonetPref.svelte";
+	import MonetPref from "../components/MonetPref/MonetPref.svelte";
+	import "../assets/global.css";
+
+	export let options: Options = [];
+	export let onChange = (
+		detail: { type: "allow" | "deny"; name: string } | { type: "reorder" },
+	) => {};
+</script>
+
+<MonetPref bind:options on:change={(ev) => onChange(ev.detail)} />
