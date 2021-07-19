00c3c553e4a6d448167f3e57b8d3f95770f612f5
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2021-07-19 20:42:27 +0530

    feat(components/monet-inline): expose all props

--- src/routes/components/monet-inline.svelte
+++ src/routes/components/monet-inline.svelte
@@ -10,24 +10,47 @@
 			['dark', 'Dark'],
 			['dim', 'Dark (dim)'],
 		],
-		value(val) {
-			if (!val) return undefined;
-			return val;
-		},
 	};
+	const heading1Options = { name: 'heading1', default: defaults.heading1 };
+	const heading2Options = { name: 'heading2', default: defaults.heading2 };
+	const subtitleOptions = { name: 'subtitle', default: defaults.subtitle };
+	const bodyOptions = { name: 'body', default: defaults.body };
+	const ctaTextOptions = { name: 'cta', prop: 'ctaText', default: defaults.ctaText };
+	const linkOptions = { name: 'link', prop: 'href', default: defaults.href };
 
 	const name = 'monet-inline';
-	const options = [themeOptions];
+	const options = [
+		themeOptions,
+		heading1Options,
+		heading2Options,
+		subtitleOptions,
+		bodyOptions,
+		ctaTextOptions,
+		linkOptions,
+	];
 </script>
 
 <script>
 	import Page from '$lib/components/components/Page.svelte';
 	import RadioGroup from '$lib/components/components/RadioGroup.svelte';
+	import Text from '$lib/components/components/Text.svelte';
 
 	let theme = themeOptions.default;
-	$: values = [theme];
+	let heading1 = heading1Options.default;
+	let heading2 = heading2Options.default;
+	let subtitle = subtitleOptions.default;
+	let body = bodyOptions.default;
+	let ctaText = ctaTextOptions.default;
+	let link = linkOptions.default;
+	$: values = [theme, heading1, heading2, subtitle, body, ctaText, link];
 </script>
 
 <Page {name} {options} {values} {markupComment}>
 	<RadioGroup bind:selected={theme} legend="Theme" name="theme" options={themeOptions.options} />
+	<Text name={ctaTextOptions.name} label="CTA Text" bind:value={ctaText} />
+	<Text name={linkOptions.name} label="CTA URL" bind:value={link} />
+	<Text name={heading1Options.name} label="First Heading" bind:value={heading1} />
+	<Text name={heading2Options.name} label="Second Heading" bind:value={heading2} />
+	<Text name={subtitleOptions.name} label="Subtitle" bind:value={subtitle} />
+	<Text name={bodyOptions.name} label="Body Text" rows={3} bind:value={body} />
 </Page>
