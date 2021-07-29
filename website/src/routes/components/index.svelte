6db745093fc368aa6a63e84079be6683f8e5471b
Matt Mankins <mankins@gmail.com>
2021-07-29 14:33:35 +0100

    Feature/copy (#5)
    * copy wip
    
    * remove wip

---
+++ website/src/routes/components/index.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Examples from '$lib/components/home/Examples.svelte';
+</script>
+
+<section class="bg-white pt-40 lg:py-20 flex flex-col gap-1">
+	<Examples />
+</section>
--- website/src/routes/index.svelte
+++ website/src/routes/index.svelte
@@ -1,11 +1,9 @@
 <script>
 	import HomeHeader from '$lib/components/home/HomeHeader.svelte';
-	import Examples from '$lib/components/home/Examples.svelte';
 </script>
 
 <section>
 	<HomeHeader />
 </section>
-<section class="bg-white pt-40 lg:py-20 flex flex-col gap-1">
-	<Examples />
-</section>
+
+  
