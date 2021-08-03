59d64276e830416c68de08b05880016013b32752
Matt Mankins <mankins@gmail.com>
2021-08-03 17:30:46 +0100

    feat(monet): add heading attrib to monetpref (#6)

--- monet/src/elements/MonetPref.svelte
+++ monet/src/elements/MonetPref.svelte
@@ -5,10 +5,11 @@
 	import MonetPref from "../components/MonetPref/MonetPref.svelte";
 	import "../assets/global.css";
 
+	export let heading = '';
 	export let options: Options = [];
 	export let onChange = (
 		detail: { type: "allow" | "deny"; name: string } | { type: "reorder" },
 	) => {};
 </script>
 
-<MonetPref bind:options on:change={(ev) => onChange(ev.detail)} />
+<MonetPref bind:options bind:heading on:change={(ev) => onChange(ev.detail)} />
