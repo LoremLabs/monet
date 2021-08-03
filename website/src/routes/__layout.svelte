55d846e17c2ea30f5bf30ba5b6df3666301963de
Matt Mankins <mankins@gmail.com>
2021-08-03 17:30:27 +0100

    Feature/analytics (#7)
    * feat(website): add fathom analytics
    
    * feat(website): fiddle with footer/nav
    
    * chore(monet): update gitignore

--- website/src/routes/__layout.svelte
+++ website/src/routes/__layout.svelte
@@ -1,22 +1,38 @@
 <script>
 	import { onMount } from 'svelte';
+	import { browser } from '$app/env';
+	import { page } from '$app/stores';
+	import * as Fathom from 'fathom-client';
+
 	import '$lib/styles/fonts.css';
 	import '$lib/styles/tailwind.css';
 	import '$lib/styles/highlight.css';
+
 	import Nav from '$lib/components/nav/Nav.svelte';
 	import Footer from '$lib/components/nav/Footer.svelte';
 
 	onMount(() => {
+		// analytics
+		Fathom.load('IABNMEHD', {
+			includedDomains: ['monet.loremlabs.com'],
+			url: 'https://swordfish.lorem.industries/script.js',
+		});
+
 		if (!window.monet) {
 			console.error('`window.monet` not found.');
 			return;
 		}
+
+		//
 		const capabilities = window.monet.capabilities.acquire();
 		capabilities.define('webmonetization/*', () => ({ isSupported: true }));
 		capabilities.define('ads/*', () => ({ isSupported: true }));
 		capabilities.define('subscription/*', () => ({ isSupported: true }));
-		capabilities.unlock();
+		capabilities.unlock(); // don't unlock if you don't want others to change
 	});
+
+	// track a page view when the URL path changes
+	$: $page.path, browser && Fathom.trackPageview();
 </script>
 
 <svelte:head>
@@ -28,4 +44,4 @@
 	<slot />
 </main>
 <Footer />
-<div class="p-4 lg:p-8"><monet-pref /></div>
+<div class="p-4 lg:p-8"><monet-pref heading="Example Monetization Preferences" /></div>
